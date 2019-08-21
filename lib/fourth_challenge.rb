4f6c5e2798aa69bb35b5756a6fae489d6af66dbf
Katrina R <katrina.reinsdorff@gmail.com>
2019-08-21 18:21:05 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,18 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        
+        "Benvolio" => {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        }
+   ],
+   [
+        "Mercutio" => {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"          
+        }
       ]
    }, 
    :capulet => {
@@ -13,16 +24,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        "Benvolio" => {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        }
-        "Mercutio" => {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "worried"          
-        }
+
       ]
    }
 }
